620a0c55e64c118d9b24df23a0cf32b8d828fff6
Sean Strout <sps@cs.rit.edu>
2019-09-03 18:27:11 -0400

    Initial student starter code with the test files and a starter main program.

---
+++ src/merchants.py
@@ -0,0 +1,28 @@
+"""
+CSAPX Lab 3: Merchants of Venice
+
+Given a list of merchants at integer locations on a road, find the optimal
+location to place a new merchant, such that the sum of the distances the
+new merchant must travel to all other merchants is minimized.
+
+$ python3 merchants.py [slow|fast] input-file
+
+Author: Sean Strout @ RIT CS
+Author: YOUR NAME HERE
+"""
+
+import collections      # namedtuple
+import sys              # arg
+import time             # clock
+import random           # random
+
+from typing import List # List
+
+def main() -> None:
+    """
+    The main function.
+    :return: None
+    """
+
+if __name__ == '__main__':
+    main()
