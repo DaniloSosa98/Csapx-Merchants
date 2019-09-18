41d9a4d1c24d801d148b65d486808e42b30ba8e6
dgs5678 <dgs5678@g.rit.edu>
2019-09-18 17:08:51 -0400

    QuickSort and Median Search

--- src/merchants.py
+++ src/merchants.py
@@ -8,21 +8,69 @@ new merchant must travel to all other merchants is minimized.
 $ python3 merchants.py [slow|fast] input-file
 
 Author: Sean Strout @ RIT CS
-Author: YOUR NAME HERE
+Author: Danilo Sosa
 """
 
 import collections      # namedtuple
+from typing import List
+from typing import Tuple
 import sys              # arg
 import time             # clock
 import random           # random
 
 from typing import List # List
 
+Merchant = collections.namedtuple( 'Merchant',
+                                 ('name', 'location') )
+
+def read_merchant( filename: str ) -> List[ Merchant ]:
+
+    merchants = list( )
+    with open( filename ) as f:
+        for line in f:
+            fields = line.split( ' ' )
+            merchants.append( Merchant(
+                name=fields[ 0 ],
+                location=int(fields[ 1 ])
+            ) )
+    return merchants
+
+def quick_sort(data: List[Merchant]) -> List[Merchant]:
+    
+    if len(data) == 0:
+        return []
+    else:
+        pivot = data[0]
+        less, equal, greater = _partition(data, pivot)
+        return quick_sort(less) + equal + quick_sort(greater)
+
+def _partition(data: List[Merchant], pivot: Merchant) \
+      -> Tuple[List[Merchant], List[Merchant], List[Merchant]]:
+    
+    less, equal, greater = [], [], []
+    for element in data:
+        if element.location < pivot.location:
+            less.append(element)
+        elif element.location > pivot.location:
+            greater.append(element)
+        else:
+            equal.append(element)
+    return less, equal, greater
+
 def main() -> None:
     """
     The main function.
     :return: None
     """
+    merchants = read_merchant("./data/test-10.txt")
+    sorted_merchants = quick_sort(merchants)
+    for merchant in sorted_merchants:
+        print(merchant)
+    
+    median = len(merchants)//2
+    print('\n')
+
+    print(sorted_merchants[median])
 
 if __name__ == '__main__':
     main()
